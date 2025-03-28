7f424bace9de4fcbf59c5f979f9f3e8f487abdc5
Leandro <leandrokatlux@icloud.com>
2025-03-28 15:15:46 -0300

    Primeiro commit

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,17 +1,21 @@
 import '../styles/Menu.css';
 
 function Menu() {
-    return (
-      <div className="Menu">
-        <ul>
-          <li><button>Calulndo Alguma Coisa</button></li>
-          <li><button>Calulndo Alguma Coisa</button></li>
-          <li><button>Calulndo Alguma Coisa</button></li>
-          <li><button>Calulndo Alguma Coisa</button></li>
-          
-        </ul>
-      </div>
-    );
-  }
-  
-  export default Menu;
+  return (
+    <div className="Menu">
+      <ul>
+        <li>
+          <button>Calulndo Alguma Coisa</button>
+        </li>
+        <li>
+          <button>Calulndo Alguma Coisa</button>
+        </li>
+        <li>
+          <button>Calulndo Alguma Coisa</button>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Menu;
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,12 +1,11 @@
 import '../styles/Title.css';
 
 function Title() {
-    return (
-      <header>
-        <h1>Através dos cards, Aprenda, Lembre, Pratique</h1>
-      </header>
-    );
-  }
-  
-  export default Title;
-  
+  return (
+    <header>
+      <h1>Através dos cards, Aprenda, Lembre, Pratique</h1>
+    </header>
+  );
+}
+
+export default Title;
